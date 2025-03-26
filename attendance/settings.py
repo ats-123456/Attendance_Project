2000e7b526b32511c6b0d903a25451a8de17bfa2
ats-123456 <132741377+suresh-27-09@users.noreply.github.com>
2025-03-26 16:50:19 +0530

    uu

--- attendance/settings.py
+++ attendance/settings.py
@@ -79,12 +79,22 @@ WSGI_APPLICATION = 'attendance.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('postgresql://attendance_db_d7ej_user:QMdyzWs6Eksm6GEg7tDNRlC1CaRChF48@dpg-cvhtqn9opnds73fnqp10-a/attendance_db_d7ej'))
 }
+
+
 
 
 # Password validation
